c8f82d13bb4a53f23022ec8002eae387d75ac856
Ravi dwivedi <ravi.dwivedi9205@gmail.com>
2022-10-03 16:26:22 +0530

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Ravi Dwivedi on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
